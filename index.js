338f616b77cc937b8e250daf2362818c5a0a12db
Amaterasus <skyetay03@googlemail.com>
2020-03-29 03:54:05 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+const num1 = 1
+const num2 = 62
+let multiply = num1 * num2
+const random = Math.random()
+const mod = 24 % 5
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
